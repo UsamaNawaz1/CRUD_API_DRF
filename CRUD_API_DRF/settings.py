f6132bd5ec03b91c4439825f25ec690514e16b18
Usama Nawaz <usamanawaz1994@gmail.com>
2021-07-15 02:35:32 -0700

    Adding permissions

--- CRUD_API_DRF/settings.py
+++ CRUD_API_DRF/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!u8!0-vmiopf%tvgmn4r7@=jx-8p&ws$!wcger0s+8)eua(8-r'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import post
 from .serializers import PostSerializer
+from .permissions import AuthorOrReadOnly
 
 class PostList(generics.ListCreateAPIView):
+    
     queryset = post.objects.all()
     serializer_class = PostSerializer
 
 class DetailPost(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (AuthorOrReadOnly,)
     queryset = post.objects.all()
     serializer_class = PostSerializer
